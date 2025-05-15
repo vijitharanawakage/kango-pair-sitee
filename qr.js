9a1df9d9f30379a752e7351eb31fd2daa91b2e20
OfficialKango <kangonathaniel@gmail.com>
2025-05-15 23:28:10 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -11,7 +11,7 @@ const {
     makeCacheableSignalKeyStore,
     Browsers,
     jidNormalizedUser
-} = require("@whiskeysockets/baileys");
+} = require("@fizzxydev/baileys-pro");
 const { upload } = require('./mega');
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
