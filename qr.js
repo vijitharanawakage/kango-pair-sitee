d32248f37ad8581612c853601c4fb78c988f0601
OfficialKango <kangonathaniel@gmail.com>
2025-05-15 23:51:37 +0000

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,17 +1,10 @@
 const { makeid } = require('./gen-id');
 const express = require('express');
-const QRCode = require('qrcode');
 const fs = require('fs');
 let router = express.Router();
 const pino = require("pino");
-const {
-    default: makeWASocket,
-    useMultiFileAuthState,
-    delay,
-    makeCacheableSignalKeyStore,
-    Browsers,
-    jidNormalizedUser
-} = require("@fizzxydev/baileys-pro");
+const { default: makeWASocket, useMultiFileAuthState, delay, Browsers, makeCacheableSignalKeyStore, getAggregateVotesInPollMessage, DisconnectReason, WA_DEFAULT_EPHEMERAL, jidNormalizedUser, proto, getDevice, generateWAMessageFromContent, fetchLatestBaileysVersion, makeInMemoryStore, getContentType, generateForwardMessageContent, downloadContentFromMessage, jidDecode } = require('@whiskeysockets/baileys')
+
 const { upload } = require('./mega');
 function removeFile(FilePath) {
     if (!fs.existsSync(FilePath)) return false;
@@ -19,7 +12,7 @@ function removeFile(FilePath) {
 }
 router.get('/', async (req, res) => {
     const id = makeid();
- //   let num = req.query.number;
+    let num = req.query.number;
     async function GIFTED_MD_PAIR_CODE() {
         const {
             state,
@@ -34,23 +27,32 @@ function selectRandomItem(array) {
 var randomItem = selectRandomItem(items);
             
             let sock = makeWASocket({
-                	
-				auth: state,
-				printQRInTerminal: false,
-				logger: pino({
-					level: "silent"
-				}),
-				browser: Browsers.macOS("Desktop"),
-			});
-            
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                generateHighQualityLinkPreview: true,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                syncFullHistory: false,
+                browser: Browsers.macOS(randomItem)
+            });
+            if (!sock.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await sock.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
+                }
+            }
             sock.ev.on('creds.update', saveCreds);
             sock.ev.on("connection.update", async (s) => {
-                const {
+
+    const {
                     connection,
-                    lastDisconnect,
-                    qr
+                    lastDisconnect
                 } = s;
-              if (qr) await res.end(await QRCode.toBuffer(qr));
+                
                 if (connection == "open") {
                     await delay(5000);
                     let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
@@ -67,6 +69,9 @@ var randomItem = selectRandomItem(items);
                     }
                     const randomText = generateRandomText();
                     try {
+
+
+                        
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
@@ -86,12 +91,12 @@ Dont forget to fork the repo ⬇️
 
 https://github.com/OfficialKango/KANGO-XMD
 
-> *© Powered BY Hector Manuel 🖤*`;
+> *© Powered BY Hector Manuel 🖤*`; 
                         await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: "𝕶𝖆𝖓𝖌𝖔 𝖇𝖔𝖙 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉",
+title: "Hector Manuel",
 thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
 sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
 mediaType: 1,
@@ -102,26 +107,12 @@ renderLargerThumbnail: true
 {quoted:code })
                     } catch (e) {
                             let ddd = sock.sendMessage(sock.user.id, { text: e });
-                            let desc = `*Hello there KANGO-XMD User! 👋🏻* 
-
-> Do not share your session id with anyone.
-
- *Thanks for using KANGO-XMD 🚩* 
-
-> Join WhatsApp Channel :- ⤵️
- 
-https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E
-
-Dont forget to fork the repo ⬇️
-
-https://github.com/OfficialKango/KANGO-XMD
-
-> *© Powered BY Hector Manuel 🖤*`;
+                            let desc = `*Don't Share with anyone this code use for deploy KANGO-XMD*\n\n ◦ *Github:* https://github.com/OfficialKango/KANGO-XMD`;
                             await sock.sendMessage(sock.user.id, {
 text: desc,
 contextInfo: {
 externalAdReply: {
-title: " 𝕶𝖆𝖓𝖌𝖔 𝖇𝖔𝖙 𝖈𝖔𝖓𝖓𝖊𝖈𝖙𝖊𝖉✅  ",
+title: "KANGO-XMD",
 thumbnailUrl: "https://i.imgur.com/GVW7aoD.jpeg",
 sourceUrl: "https://whatsapp.com/channel/0029Va8YUl50bIdtVMYnYd0E",
 mediaType: 2,
@@ -151,10 +142,10 @@ showAdAttribution: true
             }
         }
     }
-    await GIFTED_MD_PAIR_CODE();
-});
+   return await GIFTED_MD_PAIR_CODE();
+});/*
 setInterval(() => {
     console.log("☘️ 𝗥𝗲𝘀𝘁𝗮𝗿𝘁𝗶𝗻𝗴 𝗽𝗿𝗼𝗰𝗲𝘀𝘀...");
     process.exit();
-}, 180000); //30min
+}, 180000); //30min*/
 module.exports = router;
